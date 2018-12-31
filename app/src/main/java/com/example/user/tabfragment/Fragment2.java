a2c4dd0e4437630a7228484a8478351caa784405
Your Name <hbbr@kaist.ac.kr>
2018-12-31 23:17:08 +0900

    push

--- app/src/main/java/com/example/user/tabfragment/Fragment2.java
+++ app/src/main/java/com/example/user/tabfragment/Fragment2.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,33 +19,17 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Fragment2 extends Fragment {
 
-    public Fragment2() {
-        // Required empty public constructor
-        Log.i("fragment2", "empty");
+    public Fragment2(){
     }
 
-    @Override //fragment가 생성될때 호출되는 부분
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i("fragment2", "onCreate");
-
-    }
-
-
-    @Override //onCreate 후에 화면을 구성할 때 호출되는 부분
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Log.i("fragment2","onCreateView");
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment2, container, false);
+        Log.i("fragment2","onCreateView");
         if(checkPermissionREAD_EXTERNAL_STORAGE(getActivity())) {
             GridView gv = view.findViewById(R.id.ImgGridView);
-            Log.i("fragment2", "permission pass");
             final ImageAdapter ia = new ImageAdapter(getActivity());
             gv.setAdapter(ia);
             gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -58,9 +41,11 @@ public class Fragment2 extends Fragment {
         return view;
     }
 
+    /*Permission*/
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
+        Log.i("fragment2","onRequestPermissionsResult");
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -82,10 +67,7 @@ public class Fragment2 extends Fragment {
             final Context context) {
         Log.i("fragment2","checkPermissionREAD_EXTERNAL_STORAGE");
         int currentAPIVersion = Build.VERSION.SDK_INT;
-        Log.i("fragment2","currentAPIVersion : "+currentAPIVersion + " android ... : "+android.os.Build.VERSION_CODES.M);
-        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
-            Log.i("fragment2","firstOne : "+ContextCompat.checkSelfPermission(context,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) + " secondOne : "+PackageManager.PERMISSION_GRANTED);
+        if (currentAPIVersion >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(context,
                     Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(
@@ -113,6 +95,7 @@ public class Fragment2 extends Fragment {
 
     public void showDialog(final String msg, final Context context,
                            final String permission) {
+        Log.i("fragment2","showDialog");
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
         alertBuilder.setCancelable(true);
         alertBuilder.setTitle("Permission necessary");
--- app/src/main/java/com/example/user/tabfragment/MainActivity.java
+++ app/src/main/java/com/example/user/tabfragment/MainActivity.java
@@ -1,105 +1,130 @@
 package com.example.user.tabfragment;
 
-import android.content.Intent;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private final int FRAGMENT1 = 1;
-    private final int FRAGMENT2 = 2;
-    private final int FRAGMENT3 = 3;
 
 
-    private Button bt_tab1, bt_tab2, bt_tab3;
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+
+    //ViewPager에는 한번에 하나의 섹션만 보여진다.
+    static ViewPager mViewPager;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //어댑터를 생성한다. 섹션마다 프래그먼트를 생성하여 리턴해준다.
+        mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
 
+        //액션바를 설정한다.
+        final ActionBar actionBar = getSupportActionBar();
 
-        // 위젯에 대한 참조
-        bt_tab1 = (Button)findViewById(R.id.bt_tab1);
-        bt_tab2 = (Button)findViewById(R.id.bt_tab2);
-        bt_tab3 = (Button)findViewById(R.id.bt_tab3);
+        //액션바 코너에 있는 Home버튼을 비활성화 한다.
+        actionBar.setHomeButtonEnabled(true);
 
+        //탭을 액션바에 보여줄 것이라고 지정한다.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        // 탭 버튼에 대한 리스너 연결
-        bt_tab1.setOnClickListener(this);
-        bt_tab2.setOnClickListener(this);
-        bt_tab3.setOnClickListener(this);
+        //ViewPager를 정의하고
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        //ViewPager에 어댑터를 연결한다.
+        mViewPager.setAdapter(mAppSectionsPagerAdapter);
 
-        if(getIntent().getIntExtra("status",0)==312) {
-            Log.i("notice","back!");
-            callFragment(FRAGMENT2);
+        //사용자가 섹션사이를 스와이프할때 발생하는 이벤트에 대한 리스너를 설정한다.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override //스와이프로 페이지 이동시 호출됨
+            public void onPageSelected(int position) {
+                //액션바의 탭위치를 페이지 위치에 맞춘다.
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        //각각의 섹션을 위한 탭을 액션바에 추가한다.
+        for (int i = 0; i <mAppSectionsPagerAdapter.getCount(); i++) {
+            Log.i("main","adding..."+i);
+            actionBar.addTab(
+                    actionBar.newTab()
+                            //어댑터에서 정의한 페이지 제목을 탭에 보이는 문자열로 사용한다.
+                            .setText(mAppSectionsPagerAdapter.getPageTitle(i))
+                            //TabListener 인터페이스를 구현할 액티비티 오브젝트도 지정한다.
+                            .setTabListener(this));
         }
-        // 임의로 액티비티 호출 시점에 어느 프레그먼트를 프레임레이아웃에 띄울 것인지를 정함
-        else
-            callFragment(FRAGMENT3);
+    }
 
+
+    @Override //액션바의 탭 선택시 호출됨
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        //액션바에서 선택된 탭에 대응되는 페이지를 뷰페이지에서 현재 보여지는 페이지로 변경한다.
+        mViewPager.setCurrentItem(tab.getPosition());
 
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bt_tab1 :
-                // '버튼1' 클릭 시 '프래그먼트1' 호출
-                callFragment(FRAGMENT1);
-                break;
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
-            case R.id.bt_tab2 :
-                // '버튼2' 클릭 시 '프래그먼트2' 호출
-                callFragment(FRAGMENT2);
-                break;
-
-            case R.id.bt_tab3 :
-                // '버튼2' 클릭 시 '프래그먼트2' 호출
-                callFragment(FRAGMENT3);
-                break;
-        }
     }
 
-    private void callFragment(int frament_no){
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
-        // 프래그먼트 사용을 위해
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+    }
 
-        switch (frament_no){
-            case 1:
-                // '프래그먼트1' 호출
-                Fragment1 fragment1 = new Fragment1();
-                transaction.replace(R.id.fragment_container, fragment1);
-                transaction.commit();
-                break;
-
-            case 2:
-                // '프래그먼트2' 호출
-                /*
-                Intent intent = new Intent(this, ImageList.class);
-                startActivity(intent);
-                */
-                ///*
-                Fragment2 fragment2 = new Fragment2();
-                transaction.replace(R.id.fragment_container, fragment2);
-                transaction.commit();
-                //*/
-                break;
-
-            case 3:
-                // '프래그먼트2' 호출
-                Fragment3 fragment3 = new Fragment3();
-                transaction.replace(R.id.fragment_container, fragment3);
-                transaction.commit();
-                break;
+    //세션에 대응되는 프래그먼트를 리턴한다
+    //FragmentPagerAdapter는 메모리에 프래그먼트를 로드한 상태로 유지하지만(3개 프래그먼트 유지하는게 적당함)
+    //FragmentStatePagerAdapter는 화면에 보이지 않는 프래그먼트는 메모리에서 제거한다.
+    public class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+        private FragmentManager fm;
+        public AppSectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+            this.fm = fm;
         }
 
+        @Override
+        public Fragment getItem(int pos) {
+            //태그로 프래그먼트를 찾는다.
+            Fragment fragment = fm.findFragmentByTag("android:switcher:" + mViewPager.getId() + ":" + getItemId(pos));
+
+            //프래그먼트가 이미 생성되어 있는 경우에는 리턴
+            if (fragment != null) {
+                return fragment;
+            }
+
+            Log.i("in main","getitem");
+            //프래그먼트의 인스턴스를 생성한다.
+            switch(pos) {
+                case 0: return new Fragment1();
+                case 1: return new Fragment2();
+                case 2: return new Fragment3();
+                default : return null;
+            }
+
+        }
+
+        //프래그먼트를 최대 3개를 생성할 것임
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        //탭의 제목으로 사용되는 문자열 생성
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case 0: return "Contacts";
+                case 1: return "Gallery";
+                case 2: return "Game";
+                default: return "Something Wrong";
+            }
+        }
     }
 
 
